e21c40df4def05d76954466fff5813576ed6aaed
Nirali Sanghvi <58932139+Nirali0029@users.noreply.github.com>
2020-10-14 19:44:08 +0530

    Update group_anagrams.py

--- Data_Structures/Maps_Hash_Dict/Group_Anagrams/group_anagrams/group_anagrams.py
+++ Data_Structures/Maps_Hash_Dict/Group_Anagrams/group_anagrams/group_anagrams.py
@@ -1,7 +1,11 @@
+from itertools import groupby
+print("Please input the required data seperated with space")
+test_list=input("Enter data:").split()
 
-test_list = ['lump', 'eat', 'me', 'tea', 'em', 'plum'] 
+
 temp = lambda test_list: sorted(test_list) 
 res = [list(val) for key, val in groupby(sorted(test_list, key = temp), temp)] 
 
 # print result 
 print("The grouped Anagrams : " + str(res)) 
+x 
